ba9ca9cf113e66e41eab1f40f5079b8ec655a7e0
kazuhirokajimoto <kazuhiro.kajimoto@woven-planet.global>
2023-02-09 18:58:40 +0900

    addNpc

---
+++ Assets/RpgAdventure/Scripts/Quest/Quest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using UnityEngine;
+
+namespace RpgAdventure
+{
+    public enum QuestType
+    {
+        HUNT,
+        GATHER,
+        TALK,
+        EXPLORE
+    }
+    [System.Serializable]
+    public class Quest
+    {
+        public string uid;
+        public string title;
+        public string description;
+        public int experience;
+        public int gold;
+
+        public int amount;
+        public string[] target;
+
+        public string talkTo;
+        public Vector3 explor;
+
+        public string questGiver;
+        public QuestType Type;
+    }
+}
---
+++ Assets/RpgAdventure/Scripts/Quest/QuestManager.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+
+namespace RpgAdventure
+{ 
+
+    public class JsonHelper
+    {
+        private class Wrapper<T>
+        {
+            public T[] array;
+        }
+        public static T[] GetJsonArray<T>(string json)
+        {
+            string newJson = "{ \"array\": " + json + "}";
+            Wrapper<T> wrapper = JsonUtility.FromJson<Wrapper<T>>(newJson);
+            return wrapper.array;
+        }
+      }
+    public class QuestManager : MonoBehaviour
+    {
+        public Quest[] quests;
+
+        private void Awake()
+        {
+            LoadQuestsFromDB();
+        }
+
+        private void LoadQuestsFromDB()
+        {
+             using StreamReader reader = new StreamReader("Assets/RpgAdventure/DB/QuestDB.json");
+             string json = reader.ReadToEnd();
+             var loadedQuests = JsonHelper.GetJsonArray<Quest>(json);
+             quests = new Quest[loadedQuests.Length];
+             quests = loadedQuests;
+        }
+    }
+}
+ 
+
